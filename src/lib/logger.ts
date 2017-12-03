36be4faaa3562b86de5c1fe3862424777ed7787f
karaxuna <kaxabernet@gmail.com>
2017-12-03 22:37:41 +0400

    logging fixed

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,3 +1,26 @@
 export function log (...args) {
-    alert(args.map(arg => JSON.stringify(arg)).join(' '));
+    alert(args.map(arg => {
+        let err = new Error();
+
+        if (typeof arg === 'string') {
+            err.message = arg;
+        }
+        else {
+            if (arg.stack) {
+                err.stack = arg.stack;
+            }
+
+            if (arg.message) {
+                err.message = arg.message;
+            }
+
+            if (arg.name) {
+                err.name =  arg.name;
+            }
+        }
+
+        return err;
+    })
+        .map(err => [err.name, err.message, err.stack].filter(data => data).join('\n'))
+        .join(' '));
 }
